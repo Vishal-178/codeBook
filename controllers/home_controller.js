2a35d9508048f5f420d65fd7b9698937fe67411a
vishalkumarmps@gmail.com <vishalkumarmps@gmail.com>
2022-03-07 15:04:27 +0530

    worked on homecontroller, create post, save post in database and display user data along with their name

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,31 @@
+
+const Posts = require('../models/post');
+
 module.exports.home = function(req,res){
-    return res.render('home',{
-        title:'Home'
-    });
+    console.log(req.user);
+    // Posts.find({user:req.user._id},function(err,posts){
+        // console.log("======================");
+        // console.log(posts);
+        // User.findById(req.user._id,function(err,users){
+        //     return res.render('home',{
+        //         title:'Home',
+        //         posts:posts,
+        //         user:users
+        //     });
+        // });
+        
+        // Populate the user of each post
+        // read mongoose doc for populate and .exec
+        Posts.find({}).populate('user').exec(function(err,posts){
+            return res.render('home',{
+                title:'Home',
+                posts:posts,
+            });
+        });
+
+    // });
+    // return res.render('home',{
+    //     title:"home"
+    // })
     // return res.end('<h1>express control is running fuin</h1>');
 }
