4d67ddb5d094d38ae426d193271acd60ef0a70cb
vishalkumarmps@gmail.com <vishalkumarmps@gmail.com>
2022-03-09 17:47:18 +0530

    new Feature --> delete comment

--- routes/comments.js
+++ routes/comments.js
@@ -5,4 +5,5 @@ const postComment = require('../controllers/comments_controller');
 
 // check wether the comment is via authenticated user or not,
 router.post('/create',passport.checkAuthentication,postComment.create)
+router.get('/destroy/:id', passport.checkAuthentication,postComment.destroy);
 module.exports = router;
