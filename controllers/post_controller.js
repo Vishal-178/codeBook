576654e354da95d9444954199df451770e335313
vishalkumarmps@gmail.com <vishalkumarmps@gmail.com>
2022-03-09 16:10:50 +0530

    new feature -> delete post

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 module.exports.post = function(req,res){
     // console.log(req.body);
     // console.log(req.user);
@@ -8,5 +9,19 @@ module.exports.post = function(req,res){
     },function(err,post){
         if(err){console.log('error in creating a post');return}
         return res.redirect('back');
+    });
+}
+
+module.exports.destroy = function(req,res){
+    Post.findById(req.params.id, function(err,post){
+        // .id means converting the object id into string.
+        if(post.user == req.user.id){
+            post.remove();
+            Comment.deleteMany({post: req.params.id}, function(err){
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
     });
 }
--- routes/posts.js
+++ routes/posts.js
@@ -5,5 +5,5 @@ const postController = require('../controllers/post_controller');
 // after /posts
 // check wether the post is via authenticated user or not.
 router.post('/create',passport.checkAuthentication, postController.post);
-
+router.get('/destroy/:id',passport.checkAuthentication,postController.destroy);
 module.exports = router;
