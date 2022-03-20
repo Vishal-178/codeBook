f84d19f07214dd179c92dfda16905a639a967a79
Vishal Singh <vishalkumarmps@gmail.com>
2022-03-20 12:23:52 +0530

    created passport-jwtStrategy, generating token

---
+++ controllers/api/v1/user_api.js
@@ -0,0 +1,25 @@
+const { cache } = require('ejs');
+const User = require('../../../models/user');
+const jwt = require('jsonwebtoken');
+// auth or createSection
+module.exports.createSession =async function(req,res){
+    try{
+        let user =await User.findOne({email:req.body.email});
+        if(!user || user.password != req.body.password){
+            return res.json(422,{
+                message: "Invalid user name or password"
+            });
+        }
+        return res.json(200,{
+            message: "Sign in successfull, here is your token please keep it safe",
+            data: {
+                token: jwt.sign(user.toJSON(), 'codebook', {expiresIn:'10000'})
+            }
+        })
+    }catch(err){
+        console.log('*****',err);
+        return res.json(500,{
+            message: "Internal Server error"
+        });
+    }
+}
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/posts', require('./post'));
+router.use('/users', require('./users'));
 
 module.exports = router;
